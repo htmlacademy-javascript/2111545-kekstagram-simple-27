d40a1646a505aadcd6b009d550d38f610d42f8a3
Katya <ekaterina589273@yandex.ru>
2022-10-13 17:16:26 +0800

    Изменяет название функции генерации числ, добавляет округление дробных чисел

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-// Генерирвция случайного целого числа в диапазоне (min, max)
-function randomIntInRange (min = 0, max = 1000) {
+// Генерация случайного целого числа в диапазоне (min, max)
+function getRandomIntNumber (min = 0, max = 1000) {
   if (min < 0 || max <= 0 || min === max || min > max) {
     return NaN;
   }
+
+  // Округление дробного числа вверх
+  min = Math.ceil(min);
+  // Округление дробного числа вниз
+  max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -12,5 +17,5 @@ function checkStringMaxLength (str, maxLength = 100) {
   return str.length <= maxLength;
 }
 
-randomIntInRange(20, 50);
-checkStringMaxLength('Мяу', 10);
+getRandomIntNumber (20, 50);
+checkStringMaxLength ('Мяу', 10);
