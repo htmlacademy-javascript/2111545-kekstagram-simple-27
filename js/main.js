3769c04532677abbba82792f3b6813f9ca143b04
Katya <ekaterina589273@yandex.ru>
2022-11-15 18:56:32 +0800

    Добавляет взаимодействие с сервером

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { renderPhotos } from './render-photos.js';
+//import { renderPhotos } from './render-photos.js';
 import { openEditor, setUserFormSubmit } from './form.js';
 import './photo-effect.js';
 import './api.js';
